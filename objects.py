810540f42ddb92fcdb9866ed1a229fcacd6740b1
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2011-05-22 01:23:28 +0200

    Rewrite!

---
+++ objects.py
@@ -0,0 +1,138 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+"""Document objects returned by various scraping-functions."""
+
+from scraping.toolkit import dictionary
+from scraping.html2text import html2text
+
+from lxml import html
+
+import copy
+import types
+
+class Document(object):
+    """Object representing an document. No properties are
+    forced upon constructing. This is the base for all other
+    objects.
+
+    __getattr__ and __setattr__ will raise errors when not
+    initialized."""
+    _id = None
+
+    def __init__(self, parent=None):
+        """@param parent: """
+        object.__setattr__(self, '_properties', dict())
+        object.__setattr__(self, '_parent', parent)
+
+        if parent and not isinstance(parent, self.__class__):
+            raise ValueError("`parent` should inherit from Document.")
+
+    def __setattr__(self, name, value):
+        if not hasattr(self, name) or name in self._properties:
+            self._properties[name] = value
+        else:
+            object.__setattr__(self, name, value)
+
+    def __getattr__(self, name):
+        props = self.__dict__['_properties']
+        if name in props:
+            return props[name]
+        else:
+            raise AttributeError(name)
+
+    def __delattr__(self, name):
+        if name in self._properties:
+            del self._properties[name]
+        else:
+            raise AttributeError(name)
+
+    def getprops(self):
+        """Get properties as a dict"""
+        return self._properties
+
+    def getparent(self):
+        return self._parent
+
+    def updateprops(self, dic):
+        """Update properties.
+
+        @type dic: dictionary
+        @param dic: dictionary to use to update the properties"""
+        self.__dict__['_properties'].update(dic)
+
+    def copy(self, cls=None, parent=None):
+        """Returns a copy of itself, with all the properties deep-copied."""
+        parent = parent or self._parent
+        cls = cls or Document
+
+        d = cls(parent=parent)
+        for k,v in self._properties.items():
+            setattr(d, k, v)
+        #for k,v in copy.deepcopy(self._properties).items():
+        #    setattr(d, k, v)
+        #    #d._properties[k] = v
+        #    #d.__setattr__(k, v)
+
+        return d
+
+    def prepare(self, processor):
+        """This method prepares the document for processing. See HTMLDocument for
+        sample usage."""
+        pass
+
+class HTMLDocument(Document):
+    """Document object for HTML documents. This means that all properties are converted to
+    MarkDown compatible text in `getprops`. Moreover, lxml.html objects (or even lists of
+    lxml.html objects) are converted to text before returning."""
+    doc = None # Used to store lxml object
+
+    def _convert(self, val):
+        t = type(val)
+
+        if t is str:
+            return val.strip()
+
+        if t is html.HtmlElement:
+            return html2text(html.tostring(val, encoding=str)).strip()
+
+        if t in (list, tuple, types.GeneratorType):
+            """Check if all objects in list are HtmlElement and then proceed"""
+            val = tuple(val)
+
+            if all([type(e) is html.HtmlElement for e in val]):
+                return "\n\n".join(map(self._convert, val))            
+
+        # Unknown type
+        return val
+
+    def copy(self, parent=None):
+        d = super(HTMLDocument, self).copy(cls=HTMLDocument, parent=parent)
+        d.doc = self.doc
+        return d
+
+    @dictionary
+    def getprops(self):
+        """Return properties converted (where applicable) to MarkDown"""
+        for k,v in self._properties.items():
+            yield (k, self._convert(v))
+
+    def prepare(self, processor):
+        if self.doc is None and hasattr(self, 'url'):
+            if hasattr(processor, 'get'):
+                self.doc = processor.get(self.url)
---
+++ processors.py
@@ -0,0 +1,238 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+"""This module contains the main scraping-logic. All scrapers have to inherit
+from BaseScraper, which provides an multithreaded scraping environment."""
+
+from functools import partial
+from urllib import request
+from lxml import html
+
+import os, time
+import configparser
+import queue
+import multiprocessing
+import urllib
+import datetime
+import threading
+
+class InheritError(Exception):
+    pass
+
+class Scraper(object):
+    """Base scraper object.
+
+    Documentation:
+     * TODO
+
+     """
+    def __init__(self, exporter, max_threads=5):
+        """
+        @type exporter: scraping.exporters.builtin.Exporter
+        @param exporter: Exporter to use. Make sure this exporter is initialized.
+
+        @type max_threads: integer
+        @param max_threads: maximum amount of concurrent threads. Some scrapers (filesystem)
+        may force this value to 1.
+        """
+        self.alive = False
+        self.exporter = exporter
+
+        self.commit_queue = queue.Queue()
+        self.min_threads = threading.activeCount()
+        self.max_threads = max_threads + self.min_threads
+
+        self.start()
+
+    def _wait(self, threads=None):
+        threads = threads or self.max_threads
+        while threading.activeCount() > threads:
+            pass
+
+    def _commit(self):
+        """This function commits all documents/comments in commit_queue. To
+        stop the loop, call `quit`."""
+        print('start!')
+        while not self.commit_queue.empty() or self.alive:
+            # Keep running until all articles are committed, despite not
+            # being alive. 
+            try:
+                art = self.commit_queue.get(timeout=0.5)
+            except queue.Empty:
+                continue
+
+            self.exporter.commit(art)
+            self.commit_queue.task_done()
+
+    def _getContent(self, page):
+        """_getContent is called in a seperate thread for each document object
+        yielded by getPages"""
+        page.prepare(self)   
+             
+        for page in self.getDocumentPages(page):
+            page.prepare(self)
+            
+            doc = self.getDocument(page)
+            self.commit_queue.put(doc)
+            
+            # Get comments
+            comm = doc.copy(parent=doc)
+            for c in self.getComments(comm):
+                self.commit_queue.put(c)
+
+    def start(self):
+        """Start commit-loop"""
+        target = partial(self._commit)
+        self.commit_thread = threading.Thread(target=target)
+        self.commit_thread.start()
+
+        self.alive = True
+
+    ### SCRAPE FUNCTIONS ###
+    def getPages(self, date):
+        """Get article index-pages.
+        
+        @type date: datetime.datetime object or None
+        @param date: date to get pages for
+        
+        @return: an iterable containing instances of Article objects
+        containing at least an url property. Objects are passed separately
+        to getDocumentPages()."""
+        raise InheritError("Scrapers must override this method.")
+        
+    def getDocumentPages(self, page):
+        """Get article pages
+        
+        @type page: Document object
+        @param page: Page containing an index with references to document-pages"""
+        return [page,]
+        
+    def getDocument(self, page):
+        """Get article texts.
+        
+        @type page: Document object
+        @return: Document object or None"""
+        raise InheritError("Scrapers must override this method.")
+        
+    def getComments(self, comment):
+        """For each article returned, this function is triggered.
+        
+        @type comment: comment object
+        @param comment: comment object based on document returned
+        by getDocument."""
+        return []
+
+    ### PUBLIC FUNCTIONS ###
+    def scrape(self, date=None):
+        """Scrape all articles for `date`. If None, scraper should error or
+        (preferably) fetch all available articles.
+        
+        Warning: function may quite before all documents are committed,
+        though you're free to call this function again. Please call
+        `quit` to make sure all is committed."""
+        for p in self.getPages(date):
+            self._wait()
+            
+            part = partial(self._getContent, p)
+            threading.Thread(target=part).start()
+
+    def quit(self, close_exporter=True):
+        """Quit commit loop. *Must* be called after scraping.
+
+        @type close_exporter: Boolean
+        @param close_exporter: Wether or not to close the exporter. Defaults to True."""
+        self._wait(self.min_threads+1)
+        self.alive = False
+        self.commit_thread.join()
+
+        if close_exporter:
+            self.exporter.close()
+
+class HTTPScraper(Scraper):
+    """HTTPScraper extends a normal scraper with some utilities for web scraping."""
+    def __init__(self, exporter, max_threads=None):
+        karg = dict() if not max_threads else dict(max_threads=max_threads)
+        super(HTTPScraper, self).__init__(exporter, **karg)
+
+        self.scraped_urls = []
+        self.session = self._createSession()
+        self.login()
+        
+    def _createSession(self):
+        c = request.HTTPCookieProcessor()
+        s = request.build_opener(c)
+        request.install_opener(s)
+                
+        return s
+
+    def _getContent(self, page):
+        """This scraper fetches the contents of a webpage, and parses them using lxml."""
+        if page.url in self.scraped_urls:
+            return
+
+        self.scraped_urls.append(page.url)
+        super(HTTPScraper, self)._getContent(page)
+    
+    def login(self):
+        """Alter session to hold login credentials"""
+        pass
+
+    def filter(self, docs, date):
+        def todate(d):
+            if type(d) == datetime.datetime:
+                return d.date()
+            return d
+
+        if date:
+            date = todate(date)
+            for doc in docs:
+                if todate(doc.date) > date:
+                    continue
+                elif todate(doc.date) == date:
+                    yield doc
+                else:
+                    break
+        else:
+            for doc in docs: yield doc
+
+            
+
+
+    def get(self, url, read=True, lxml=True, attempt=0):
+        """`get` makes three attempts to retrieve the given url.
+        
+        PS: Blame VK. """
+        try:
+            fo = self.session.open(url)
+            if not read:
+                res = fo
+            elif not lxml:
+                res = fo.read()
+            else:
+                res = html.parse(fo).getroot()
+
+            print('Retrieved "%s"' % url)
+            return res
+
+        except urllib.error.URLError as e:            
+            if attempt > 3:
+                raise(e)
+
+            time.sleep(1.5)
+                
+            return self.get(url, attempt=attempt+1, read=read, lxml=lxml)
