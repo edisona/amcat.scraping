f2e0aa593cb88732096212dd135c44dacfce8bc8
Toon Alfrink <toon.alfrink@gmail.com>
2012-05-23 20:51:09 +0200

    nearly finished tubantia scraper and added wegenertools.py, which contains methods to be used for other wegener newspapers
    note that tubantia.py is not finished yet: i had problems with _login()
    which are to be solved soon.

--- news/tubantia.py
+++ news/tubantia.py
@@ -21,14 +21,92 @@ from __future__ import unicode_literals, print_function, absolute_import
 
 from amcat.scraping.scraper import DBScraper, HTTPScraper
 from amcat.scraping.document import HTMLDocument, IndexDocument
+from amcat.scraping import toolkit
+import wegenertools
+
+
+from urllib import urlencode
+#from urlparse import urljoin
+
+INDEX_URL = "http://tubantia.ned.newsmemory.com/eebrowser/frame/develop.4979.enea.1/load/newspaper.php?pSetup=tubantia&userid=9635&date=0@/tubantia/{year}{month}{day}"
+
+PAGE_URL = "http://tubantia.ned.newsmemory.com/eebrowser/frame/develop.4979.enea.1/php-script/fullpage.php?pSetup=tubantia&file=0@/tubantia/{year}{month}{day}/{pagefile}/&section={section}&edition={edition}&pageNum={pagenum}"
+
+LOGIN_URL = "http://tubantia.ned.newsmemory.com/eebrowser/frame/develop.4979.enea.1/protection/login.php?pSetup=tubantia"
+
 
 class TubantiaScraper(HTTPScraper, DBScraper):
     medium_name = "Tubantia"
 
-    def _get_units(self):
-        
+    def __init__(self, *args, **kwargs):
+        super(TubantiaScraper, self).__init__(*args, **kwargs)
 
+
+    def _login(self, username, password):
+        """log in on the web page
+        @param username: username to log in with
+        @param password: password 
+        """
+        page = self.getdoc(LOGIN_URL)
+        print(page.text_content())
+        form = stoolkit.parse_form(page)
+        form["username"] = str(username)
+        form["password"] = str(password)
+        page = self.opener.opener.open(LOGIN_URL, urlencode(form))
+
+    def _get_units(self):
+        """papers are often organised in blocks (pages) of articles, this method gets the blocks, articles are to be gotten later"""
+
+
+
+        
+        year=self.options['date'].year
+        month='0'+str(self.options['date'].month)
+        day=self.options['date'].day
+
+        INDEXURL = INDEX_URL.format(year=year,month=month,day=day)
+        index_text = str(self.getdoc(INDEXURL).text_content())
+        cur = index_text.find("p[i++]")
+        self.page_data = []
+        while index_text.find("p[i++]",cur+1) >= 0:
+            start = cur
+            cur = index_text.find("p[i++]",cur+1) #find next occurrence
+            end = cur
+            start = index_text.find("(",start,end)
+            end = index_text.find(")",start,end)
+            args = index_text[start:end].split('","')
+            pagefile = args[0].strip('("').lower()
+            section = args[1]
+            pagenum = args[2].lstrip("0")
+            edition = args[3]
+            url = PAGE_URL.format(**locals())
+            self.page_data.append(dict(pagefile=args[0].strip('("'),section=args[1],pagenum=args[2],edition=args[3],url=url))
+
+        for page in self.page_data:
+            yield page
+
+        
     def _scrape_unit(self, ipage):
+        page = ipage
+        ipage = HTMLDocument(ipage)
+        ipage.doc = self.getdoc(page['url'])
+        ipage.page = page['pagenum']
+        ipage.props.category = page['section']
+        text = wegenertools.clean(ipage.doc.text_content())
+
+        for article_ids in wegenertools.get_article_ids(text):
+            text,headline,byline = wegenertools.get_article(text,article_ids)
+            artpage = HTMLDocument()
+            artpage.props.text = text
+            artpage.props.headline = headline
+
+            yield artpage
+
+            ipage.addchild(page)
+            
+        yield ipage
+
+
 
 
 
@@ -37,4 +115,5 @@ if __name__ == '__main__':
     from amcat.tools import amcatlogging
     amcatlogging.debug_module("amcat.scraping.scraper")
     amcatlogging.debug_module("amcat.scraping.document")
-    cli.run_cli(LimburgerScraper)
+    cli.run_cli(TubantiaScraper)
+
---
+++ news/wegenertools.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+
+
+#this file can be used for:
+
+#BN DeStem
+#Brabants Dagblad
+#De Gelderlander
+#De Stentor
+#De Twentsche Courant 
+#Tubantia
+#Eindhovens Dagblad
+#PZC
+
+
+import re, collections
+from itertools import count
+
+from amcat.tools import toolkit
+
+def clean(text):
+    """Clean the javascript document before parsing"""
+    text = text.decode('latin-1')
+    text = re.sub("\x1d\\d", " ", text)
+    text = text.replace("-\x1e","")
+    text = text.replace("\x1e","")
+    text = text.replace("\x1dC"," ")
+    text = text.replace("\x1dB"," ")
+    text = re.sub("[ \t]+", " ", text)
+    return text    
+
+ARTICLE_IDS_PATTERN = r"a\[(\d+)\].e\[\d+\]=new mE\(\d+,(\d+)\);"
+
+def get_article_ids(txt):
+    """
+    Get the list of paragraph ids per article. 
+    @param txt: the cleaned js source
+    @return: a sequence of articles, where each article is a list of paragraph ids
+    """
+    articles = collections.defaultdict(list)
+    for number, parid in re.findall(ARTICLE_IDS_PATTERN, txt):
+        articles[number].append(int(parid))
+    return articles.values()
+
+       
+import htmlentitydefs
+def decode_html_entity(entity):
+    if entity.startswith("#"):
+        codepoint = int(entity[1:])
+    else:
+        codepoint = htmlentitydefs.name2codepoint[entity]
+    return unichr(codepoint)
+
+def decode_html_entities(text):
+    return re.sub('&([^;]+);', lambda m: decode_html_entity(m.group(1)), text)
+
+def _chunks_to_text(chunks):
+    text = "\n".join(chunks)
+    text = text.replace("\\n", "\n")
+    text = decode_html_entities(text)
+    text = toolkit.stripAccents(text)
+    return text.strip()
+                 
+def get_article(txt, ids):
+    """
+    Get the text for an article
+    @param txt: the cleaned js source
+    @param ids:  a list of paragraph ids to concatenate
+    @return: a string representing the text of the article
+    """
+    text = []
+    for parid in ids:
+        offset = 1
+
+        pattern = r'p\[{parid}\].S\[\d+\]=new mS\((\d+),"([^"]*)"\);$'.format(**locals())
+        m = re.findall(pattern, txt, re.MULTILINE)
+        for font, chunk in m:
+            if font == 0:
+                byline.append(chunk)
+            elif font == 1:
+                headline.append(chunk)
+            else:
+                text.append(chunk)
+
+    body, headline, byline = map(_chunks_to_text, [body, headline, byline])
+    return body, headline, byline
+
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+        
+import unittest
+
+class TestArticle(unittest.TestCase):
+    def setUp(self):
+        self.txt = open("tubantia_test.html").read()
+        
+    def test_clean(self):
+        self.assertTrue("\x1e" in self.txt)
+        cleaned = clean(self.txt)
+        self.assertFalse("\x1e" in cleaned)
+
+    def test_get_article_ids(self):
+        cleaned = clean(self.txt)
+        articles = list(get_article_ids(cleaned))
+        self.assertIn([19,20], articles)
+
+    def test_decode_html_entities(self):
+        self.assertEqual(decode_html_entity("lt"), "<")
+        self.assertEqual(decode_html_entity("#8216"), u"\u2018")
+
+        out = decode_html_entities(u'een &#8216; boardi')
+        self.assertEqual(out, u"een \u2018 boardi")
+
+
+    def test_get_article(self):
+        ids = [19, 20]
+        expected = u"""ALMELO\n\nKlinkende namen bij stadionplan Heracles\n
+Oud-KNVB-bestuurders Henk Kesler en Arie van Eijden, alsmede de oud- burgemeesters Bert Meulman (Hardenberg) en Menno Knip (Almelo) hebben zich verbonden aan het stadionplan van Heracles Almelo. De bestuurders maken deel uit van een ' boardingcommissie'. Deze kiest binnenkort een partner om het nieuwe stadion in Almelo te bouwen en te exploiteren. Vier consortia hebben daarvoor belangstelling: Ballast- Nedam, BAM, VanWijnen- Trebbe en VolkerWessels- Dura Vermeer- Bramer. De namen zijn gisteren bekend gemaakt."""
+    
+        cleaned = clean(self.txt)
+        text = get_article_text(cleaned, ids)
+
+        self.assertEqual(text, expected)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+        
