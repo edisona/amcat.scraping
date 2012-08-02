932d992e572bda9acd87c0cd6a00a943ae1014f6
toon <toon@localhost>
2012-08-02 17:39:19 +0200

    merge

--- social/_twitter.py
+++ social/_twitter.py
@@ -26,38 +26,120 @@ from django import forms
 from twitter import TwitterStream,UserPassAuth
 from datetime import date
 import json
-
+import csv
+from sys import argv
 import logging; log = logging.getLogger(__name__)
 
-class TwitterForm(ScraperForm):
-    track = forms.CharField(max_length=8192, required=False)
-    follow = forms.CharField(max_length=8192, required=False)
-    username = forms.CharField()
-    password = forms.CharField()
-
-class TwitterScraper(Scraper):
+class TwitterScraper(object):
     """Constantly get tweets from the twitter streaming api """
-    options_form = TwitterForm
-    medium_name = "Twitter"
+    username, password = argv[1:]
+    lastdate = date.today()
+    target = date.today().strftime("twitter_scrapheap_%Y-%m-%d.csv")
+    writer = csv.writer(open(target,'a+'))
 
-    def __init__(self, options):
-        super(TwitterScraper, self).__init__(options)
-
-    def run(self, options):
+    def run(self):
         
-        
-        stream = TwitterStream(auth=UserPassAuth(self.options['username'],self.options['password']))
+        stream = TwitterStream(auth=UserPassAuth(self.username,self.password))
         iterator = stream.statuses.sample()
+
+        
+        if self.lastdate != date.today():
+            print("\n\nNew date detected, changing target file\n\n")
+            self.target = date.today().strftime("twitter_scrapheap_%Y-%m-%d.csv")
+            self.writer = csv.writer(open(self.target,'a+'))
+            self.writer.writerow(['id','created at','text','hashtags','urls','user mentions','retweeted','user id','in reply to status id','in reply to user id','place id','user location','user language','contributor id\'s','contributor screen names respectively'])
+            self.lastdate = date.today()
+
+        i = 0
         for tweet in iterator:
-            target = date.today().strftime("twitter_scrapheap_%Y-%m-%d.txt")
-            target_file = open(target,'a+')
+            i += 1
+            if self.lastdate != date.today():
+                print("\n\nNew date detected, changing target file\n\n")
+                self.target = date.today().strftime("twitter_scrapheap_%Y-%m-%d.csv")
+                self.writer = csv.writer(open(self.target,'a+'))
+                self.writer.writerow(['id','created at','text','hashtags','urls','user mentions','retweeted','user id','in reply to status id','in reply to user id','place id','user location','user language','contributor id\'s','contributor screen names respectively'])
+                self.lastdate = date.today()
+            
+            
+            data =  self.filter_tweet_data(tweet)
+            if(data):
+                self.writer.writerow(data)
+
+            if i % 100 == 0:
+                print("Added {} tweets to file {}".format(i,self.target))
+
+        self.run()
+
+
+
+
+
+    def filter_tweet_data(self,tweet):
+
+
+
+        if 'id_str' not in tweet.keys():
+            return False
+
+        
+        data = [
+            tweet['id_str'],
+            tweet['created_at'],
+            tweet['text'],
+            "\n".join([hashtag['text'] for hashtag in tweet['entities']['hashtags']]),
+            "\n".join([url['url'] for url in tweet['entities']['urls']]),
+            "\n".join([user_mention['id_str'] for user_mention in tweet['entities']['user_mentions']]),
+            tweet['retweeted']
+            ]
+        try:
+            data.append(tweet['user']['id_str'])
+        except:
+            data.append(None)
+        try:
+            data.append(tweet['in_reply_to_status_id_str'])
+            data.append(tweet['in_reply_to_user_id_str'])
+        except:
+            for x in range(2):
+                data.append(" ")
+
+        try:
+            data.append(tweet['place']['id_str'])
+        except:
+            data.append(None)
+
+        if 'location' in tweet['user'].keys():
+            data.append(tweet['user']['location'])
+        else:
+            data.append(" ")
+
+        if 'lang' in tweet['user'].keys():
+            data.append(tweet['user']['lang'])
+        else:
+            data.append(" ")
+
+        try:
+            contributors = {'ids':' ','screen_names':' '}
+            for contributor in tweet['contributors']:
+                contributors['ids'] += (contributor['id_str']+";")
+                contributors['screen_names'] += (contributor['screen_name']+";")
+            data.append(contributors['ids'])
+            data.append(contributors['screen_names'])
+        except TypeError:
+            pass
+        i = 0
+        for x in data:
             try:
-                target_file.write(tweet['text']+"\n")
-            except:continue
-            log.info("Adding tweet to file {target}".format(target=target))
-        
-        
+                data[i] = unicode(x).encode('utf-8')
+                
+            except:
+                pass
+            i += 1
+        return data
+
+
+
+
 
 if __name__ == '__main__':
-    from amcat.scripts.tools import cli
-    cli.run_cli(TwitterScraper)
+    ts = TwitterScraper()
+    ts.run()
