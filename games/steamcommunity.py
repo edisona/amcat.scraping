ada27fa657e1711bb6189d1600e7c549d916e8e6
Toon Alfrink <toon.alfrink@gmail.com>
2012-10-10 14:32:46 +0200

    added unfinished steamcommunity and nieuws.nl scrapers

---
+++ games/steamcommunity.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from amcat.scraping.document import Document, HTMLDocument, IndexDocument
+
+#possibly useful imports:
+
+#from urllib import urlencode
+from urlparse import urljoin
+#from amcat.tools.toolkit import readDate
+import json
+import math
+from lxml import etree
+
+INDEX_URL = "http://steamcommunity.com/apps"
+FORUM_URL = "http://steamcommunity.com/forum/{f_id}/General/render/0/?start={start}&count=15"
+
+from amcat.scraping.scraper import HTTPScraper,DBScraper,DatedScraper,Scraper
+
+class SteamScraper(HTTPScraper, DatedScraper):
+    medium_name = "steamcommunity.com"
+
+    def __init__(self, *args, **kwargs):
+        super(SteamScraper, self).__init__(*args, **kwargs)
+
+    def _get_units(self):
+        index = self.getdoc(INDEX_URL) 
+        
+        for unit in index.cssselect('#popular1 a'):
+            href = unit.get('href')
+            app = self.getdoc(href+"/discussions")
+            for html in self.get_pages(app):
+                pass#TODO
+
+    def get_pages(self,first):
+        forum_id = first.cssselect("#AppHubContent div.leftcol div.forum_area")[0].get('id').split("_")[2]
+        total_topics = int(first.cssselect("#forum_General_3703047_pagetotal")[0].text.strip())
+        for x in range(math.floor(total_topics/15)+1):
+            page_request = self.open(FORUM_URL.format(f_id=forum_id,start=x*15))
+            html = etree.parse(json.loads(page_request.read())['topics_html'])
+            yield html
+            
+        
+    def _scrape_unit(self, ipage): 
+        pass #TODO
+
+
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.document")
+    cli.run_cli(SteamScraper)
+
+
---
+++ news/nieuws_nl.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from amcat.scraping.document import HTMLDocument
+
+#from urllib import urlencode
+#from urlparse import urljoin
+from amcat.tools.toolkit import readDate
+
+
+INDEX_URL = "http://www.nieuws.nl"
+
+from amcat.scraping.scraper import HTTPScraper,DatedScraper
+
+class Nieuws_nlScraper(HTTPScraper, DatedScraper):
+    medium_name = "nieuws.nl"
+
+    def __init__(self, *args, **kwargs):
+        super(Nieuws_nlScraper, self).__init__(*args, **kwargs)
+
+
+    def _get_units(self):
+
+        url = INDEX_URL
+        index = self.getdoc(url) 
+        
+        for unit in index.cssselect('div.submenu'):
+            href = unit.cssselect('a')[0].get('href')
+            print(href)
+            for page in self.get_pages(self.getdoc(href)):
+                i = 0
+                for _article in page.cssselect("#mainlayout_rundown .mainlayout_datum"):
+                    if readDate(_article.text_content()).date() == self.options['date']:
+                        i += 1
+                for article in page.cssselect("h2 a"):
+                    if i > 0:
+                        yield HTMLDocument(url=article.get('href'),headline=article.get('title'))
+                    i-=1
+THIS IS NOT DONE!
+                    
+    def get_pages(self, doc):
+        for page in doc.cssselect("#page_navigation_bar a"):
+            url = page.get('href')
+            yield self.getdoc(url)
+
+
+        
+    def _scrape_unit(self, article): 
+        
+        article.prepare(self)
+        article.doc = self.getdoc(article.props.url)
+        article.props.section = article.doc.cssselect("div.page_title h1")[0].text
+        article.props.date = self.options['date']
+        article.props.text = "\n".join([p.text_content() for p in article.doc.cssselect("p")])
+        yield article
+        
+
+
+
+
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.document")
+    cli.run_cli(Nieuws_nlScraper)
+
+
