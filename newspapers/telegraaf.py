dffca633c34b1e2e50aff3d6f9484845712a366c
toon <toon@localhost>
2012-10-25 15:58:13 +0200

    telegraaf fix (finally)

--- newspapers/telegraaf.py
+++ newspapers/telegraaf.py
@@ -25,9 +25,9 @@ from amcat.scraping.document import HTMLDocument, IndexDocument
 from amcat.scraping import toolkit
 from amcat.scraping.toolkit import parse_coord
 
-INDEX_URL = "http://telegraaf-i.telegraaf.nl/telegraaf/_main_/%(year)d/%(month)02d/%(day)02d/001"
-LOGIN_URL = "http://telegraaf-i.telegraaf.nl/tmg/login.php"
-
+INDEX_URL = "https://telegraaf-i.telegraaf.nl/telegraaf/_main_/%(year)d/%(month)02d/%(day)02d/001"
+LOGIN_URL = "https://telegraaf-i.telegraaf.nl/telegraaf/_main_/{y}/{m:02d}/{d:02d}/001/page.html"
+other_url = "https://telegraaf-i.telegraaf.nl/tmg/login.php"
 from urllib import urlencode
 from urlparse import urljoin
 
@@ -37,13 +37,15 @@ class TelegraafScraper(HTTPScraper, DBScraper):
     medium_name = "Telegraaf"
 
     def _login(self, username, password):
-        pagel = self.getdoc(LOGIN_URL)
-
+        l_url = LOGIN_URL.format(
+                y=self.options['date'].year,
+                m=self.options['date'].month,
+                d=self.options['date'].day)
+        pagel = self.getdoc(l_url)
         form = toolkit.parse_form(pagel)
         form["sso:field:username"] = username
         form["sso:field:password"] = password
-        pagel = self.opener.opener.open(LOGIN_URL,
-                                        urlencode(form))
+        pagel = self.open(other_url,urlencode(form))
 
         if pagel.getcode() != 200:
             raise ValueError(CREDENTIALS_ERR % pagel.getcode())
@@ -61,20 +63,17 @@ class TelegraafScraper(HTTPScraper, DBScraper):
         for td in doc.cssselect('td.select_page option'):
             url = urljoin(index, td.get('value') + '/page.html')
             page = int(td.get('value'))
-
             yield IndexDocument(url=url, page=page, date=date)
 
     def _scrape_unit(self, ipage):
         ipage.prepare(self)
-        print(ipage.doc.text_content())
-
         # Articles with an id higher than 100 are advertisements,
         # which can be filtered by excluding classnames lager than
         # 9 (articleXXX).
         articles = ipage.doc.cssselect('#page div')
         articles = set(div.get('class') for div in articles
                             if len(div.get('class')) <= 9)
-        print(articles)
+
         for clsname in articles:
             page = ipage.copy()
 
