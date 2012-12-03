71f060d69346034f2e59f05f88fabb290ad84ca1
Toon Alfrink <toon.alfrink@gmail.com>
2012-12-03 14:26:25 +0100

    no more ipages without articles, or articles with 1 line for telegraaf scraper

--- newspapers/telegraaf.py
+++ newspapers/telegraaf.py
@@ -25,6 +25,9 @@ from amcat.scraping.document import HTMLDocument, IndexDocument
 from amcat.scraping import toolkit
 from amcat.scraping.toolkit import parse_coord
 
+
+import logging; log = logging.getLogger("amcat.scraping.scraper")
+
 INDEX_URL = "https://telegraaf-i.telegraaf.nl/telegraaf/_main_/%(year)d/%(month)02d/%(day)02d/001"
 LOGIN_URL = "https://telegraaf-i.telegraaf.nl/telegraaf/_main_/{y}/{m:02d}/{d:02d}/001/page.html"
 other_url = "https://telegraaf-i.telegraaf.nl/tmg/login.php"
@@ -58,7 +61,6 @@ class TelegraafScraper(HTTPScraper, DBScraper):
         """
         date = self.options['date']
         index = INDEX_URL % dict(year=date.year, month=date.month, day=date.day)
-
         doc = self.getdoc(index)
         for td in doc.cssselect('td.select_page option'):
             url = urljoin(index, td.get('value') + '/page.html')
@@ -91,18 +93,25 @@ class TelegraafScraper(HTTPScraper, DBScraper):
             #import pdb
             #pdb.set_trace()
             page.doc = self.getdoc(page.props.url)
-            page.props.text = page.doc.cssselect('#article')
+            page.props.text = page.doc.cssselect('#article')[0].text_content()
             if page.doc.cssselect('#article h1'):
                 page.props.headline = page.doc.cssselect('#article h1')[0].text_content()
             #else implement images
 
+                if len(page.props.text.split("\n")) < 6:
+                    break
+                
                 yield page
 
+                ipage.addchild(page)
 
-        yield ipage
 
-    def get_article(self, page):
-        pass
+        if len(ipage.children) > 0:
+            data = {'ipage':ipage,'initial article set':articles}
+            log.warning("did not yield ipage, for it has no children", extra = data)
+            yield ipage
+
+
 
 if __name__ == '__main__':
     from amcat.scripts.tools import cli
