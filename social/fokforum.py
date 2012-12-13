e22f3db866d0b89ada9e84f61d805d04f4a4ed49
Toon Alfrink <toon.alfrink@gmail.com>
2012-12-13 18:23:59 +0100

    fixed fok forum scraper, it's awesome now

--- social/fokforum.py
+++ social/fokforum.py
@@ -20,22 +20,19 @@ from __future__ import unicode_literals, print_function, absolute_import
 ###########################################################################
 
 from amcat.scraping.scraper import DatedScraper, HTTPScraper
-from amcat.scraping.document import HTMLDocument
+from amcat.scraping.document import HTMLDocument, Document
 
 
 from datetime import date
 from urlparse import urljoin
 from amcat.tools.toolkit import readDate
-
+from amcat.models.article import Article
+from lxml import etree
 
 CATEGORIES_TO_SCRAPE = [
     #('forum name','forum id'),
     ('nieuws & achtergronden',4)
     ]
-
-
-
-
 
 INDEX_URL = "http://forum.fok.nl"
 
@@ -44,57 +41,78 @@ class FokForumScraper(HTTPScraper, DatedScraper):
 
     def __init__(self, *args, **kwargs):
         super(FokForumScraper, self).__init__(*args, **kwargs) 
-
-    def _login(self,username,password):
-        """Not used for logging in, but to avoid the pop up screen about cookies, which ironically needs a cookie to avoid."""
-        page = self.opener.opener.open(INDEX_URL)
+        page = self.open(INDEX_URL)
 
         cookie_string = page.info()["Set-Cookie"]
         token = cookie_string.split(";")[0]
         self.opener.opener.addheaders.append(("Cookie",token+"; allowallcookies=1"))
-        page = self.opener.opener.open(INDEX_URL)
+        page = self.open(INDEX_URL)
 
 
     def _get_units(self):
 
-        index = self.getdoc(INDEX_URL) 
-
-
         for forum,forum_id in CATEGORIES_TO_SCRAPE:
-            url = urljoin(INDEX_URL,"forum/{fid}".format(fid=forum_id))
-            yield url
+            sub_url = urljoin(INDEX_URL,"forum/{fid}".format(fid=forum_id))
+            for topic_url in self.get_topics(sub_url):
+                yield topic_url
 
+
+                
+    def get_topics(self, subforum):
+        doc = self.getdoc(subforum)
+        for div in doc.cssselect("div.mb2"):
+            table = div.cssselect("table.topiclist_bot")[0]
+            for tr in table.cssselect("tr.altcolor-post-1, tr.altcolor-post-2"):
+                date = readDate(tr.cssselect("td.tLastreply a")[0].text).date()
+                if date >= self.options['date']:
+                    if tr.cssselect("td.tOnepage a"):
+                        url = urljoin(INDEX_URL, tr.cssselect("td.tOnepage a")[0].get('href'))
+                    else:
+                        url = urljoin(INDEX_URL, tr.cssselect("td.tTitel a")[0].get('href')) + "/1/999"
+                    yield url
+                    
+                elif date < self.options['date']:
+                    break
+
+
+
+    def _scrape_unit(self, topic_url):
+        #navigate to last page, then navigate back until comments are no longer recent
+        doc = self.getdoc(topic_url)
+        headline = "".join(doc.cssselect("title")[0].text_content().split("-")[:-1])
+        topic_date = readDate(doc.cssselect("span#pt1")[0].text_content().strip())
+        try:
+            parent = Article.objects.get(headline = headline, date = topic_date)
+        except Article.MultipleObjectsReturned: #duplicate, because date is to the second
+            parents = Article.objects.filter(headline = headline, date = topic_date)
+            min_id = min([parent.id for parent in parents]) #deduplicate always keeps the lowest id
+            parent = parents.get(pk = min_id)
+        except Article.DoesNotExist:
+            parent = HTMLDocument(url = topic_url)
+            parent.props.headline = headline
+            parent.props.date = topic_date
+            parent.props.text = doc.cssselect("div.postmain_right")[0].text_content().strip()
+            parent.props.author = doc.cssselect("span.post_sub a.username")[0].text_content().strip()
         
-    def _scrape_unit(self, url):
-        doc = self.getdoc(url)
+        for post in self.get_posts(doc):
+            post.parent = parent
+            yield post
 
-        for tr in doc.cssselect("tbody#foruminsert2 tr"):
-            _date = tr.cssselect("td.tLastreply a")[0].text
-            if str(date.today()) not in str(readDate(_date)): 
-                pass
-            else:
-            
-                href = tr.cssselect("td.tTitel a")[0].get('href')
-                url = urljoin(INDEX_URL,href)
-            
-                page = HTMLDocument(date = self.options.get('date'), url = url)
-                page.prepare(self)
-            
-                page.doc = self.getdoc(page.props.url)
-                _date = page.doc.cssselect("span.post_time a")[0].text
-                if str(date.today()) in str(readDate(_date)):
-                    yield self.get_article(page)
+        if isinstance(parent, Document):
+            yield parent
 
-
-    def get_article(self, page):
-        page.props.author = page.doc.cssselect("span.post_sub a.username")[0].text
-        page.props.headline = page.doc.cssselect("div.fieldholder h1")[0].text_content()
+    def get_posts(self, doc):
+        for div in reversed(doc.cssselect("div.post")[1:]):
+            date = readDate(div.cssselect("span.post_time")[0].text_content())
+            if date.date() == self.options['date']:
+                post = Document()
+                post.props.date = date
+                post.props.author = div.cssselect("span.post_sub a.username")[0].text_content()
+                post.props.text = div.cssselect("div.postmain_right")[0].text_content().strip()
+                yield post
+            elif date.date() < self.options['date']:
+                break
         
-        page.props.text = page.doc.cssselect("div.postmain_right")[0].text_content()
-        page.coords=''
-        return page
-
-
 
 
 if __name__ == '__main__':
