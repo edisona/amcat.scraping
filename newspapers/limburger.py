50faaea7090937cc9e46bcd844692453d94b431c
Toon Alfrink <toon.alfrink@gmail.com>
2013-01-06 00:26:41 +0100

    fixed limburger scraper

--- newspapers/limburger.py
+++ newspapers/limburger.py
@@ -24,19 +24,15 @@ from amcat.scraping.document import HTMLDocument
 from amcat.scraping import toolkit as stoolkit
 import time
 from urllib import urlencode
+from lxml import html
+from urlparse import urljoin
 
-
-INDEX_URL = "http://digitaal.ddl.x-cago.net/DDL/{:%Y%m%d}/public/index_tablet.html"
-PAGE_URL = "http://digitaal.ddl.x-cago.net/DDL/{:%Y%m%d}/public/{pageurl}_tablet.html"
-ARTICLE_URL = "http://digitaal.ddl.x-cago.net/DDL/{:%Y%m%d}/public/pages/{pageid}/articles/{articleid}_body_tablet.html"
-LOGIN_URL = "http://mgl.x-cago.net/login.do?pub=ddl&auto=false"
 
 class LimburgerScraper(HTTPScraper, DBScraper):
     medium_name = "De Limburger"
-
-    def __init__(self, *args, **kwargs):
-        super(LimburgerScraper, self).__init__(*args, **kwargs)
-
+    login_url = "http://mgl.x-cago.net/login.do?pub=ddl&auto=false"
+    index_url = "http://digitaal.ddl.x-cago.net/DDL/{y:04d}{m:02d}{d:02d}/public/index.html"
+    page_url = "http://digitaal.ddl.x-cago.net/DDL/{y:04d}{m:02d}{d:02d}/public{pagelink}.html"
 
     def _login(self, username, password):
         """log in on the web page
@@ -44,61 +40,60 @@ class LimburgerScraper(HTTPScraper, DBScraper):
         @param password: password 
         """
 
-        page = self.getdoc(LOGIN_URL)
+        page = self.getdoc(self.login_url)
         form = stoolkit.parse_form(page)
         form['email'] = username
         form['password'] = password
-        page = self.opener.opener.open(LOGIN_URL, urlencode(form))
-
-    def _get_pages_links(self,page):
-        JS_text = page.text_content()
-        lines = JS_text.split(";")
-        pages = []
-        for line in lines:
-            if "pageTable.add( \"DDL" in line and not "Advertentie" in line:
-                args = line[line.find("Array"):].split(",")
-                pages.append({'date' : args[1],'link' : args[2].strip().strip('"').lstrip("/"),'pagenum' : args[3],'category' : args[6]})
-        return pages
+        page = self.getdoc(self.login_url, urlencode(form))
+        if "<b>Sessie overschrijven</b>" in html.tostring(page):
+            form = stoolkit.parse_form(page)
+            form['overwrite'] = 'on'
+            self.open("http://mgl.x-cago.net/session.do", urlencode(form))
 
     def _get_units(self):
-        """get pages"""
 
-        index_url = INDEX_URL.format(self.options['date'])
-        print(index_url)
-        index = self.getdoc(index_url)
-        
-        for page in self._get_pages_links(index):
-            url = PAGE_URL.format(self.options['date'],pageurl = page['link'])
-            yield url
+        y = self.options['date'].year
+        m = self.options['date'].month
+        d = self.options['date'].day
 
+        index_url = self.index_url.format(**locals())
+        index = self.open(index_url)
+        pages = []
+        for line in index.readlines():
+            if "pageTable.add" in line:
+                start = line.find("new Array");end = line.find(");", start)
+                print(line[start:end])
+                pagelink = line[start:end].split(",")[2].strip("\" ")
+                yield self.page_url.format(**locals())
+                
         
     def _scrape_unit(self, url):
-        doc = self.getdoc(url)
-        pageid = url.split("/")[-2]
-        for article in doc.cssselect("body div.overlay"):
+        page = self.getdoc(url)
+        correcttable = False
+        for table in page.cssselect("table"):
+            if correcttable:
+                for td in table.cssselect("td.artitem"):
+                    link = td.get('onclick').split(",")[0].lstrip("showDetails('").rstrip("'")
+                    arturl = urljoin(url, link)
+                    yield self.get_article(arturl)
+                correcttable = False
+                                                               
+            if "Artikelen op deze pagina" in table.text_content():
+                correcttable = True
 
-            text = article.text_content()
-            onclick = text[text.find("onClick"):]
-            article_id = onclick.split(",")[0].split("'")[1]
-            url = ARTICLE_URL.format(self.options['date'],pageid = pageid,articleid = article_id)
-            
-            page = HTMLDocument(date = self.options['date'], url = url)
-            page.prepare(self)
-            page.doc = self.getdoc(page.props.url)
-            yield self.get_article(page)
-
-
-    def get_article(self, page):
-        try: #not always an author in text
-            page.props.author = page.doc.cssselect("font.artauthor")[0].text.lstrip("dor")[0:98]
-        except IndexError:
-            pass
-        page.props.headline = page.doc.cssselect("font.artheader")[0].text
-        page.props.text = page.doc.cssselect("font.artbody")[0].text_content()
-        page.coords=""
-        return page
-
-
+    def get_article(self, url):
+        url = "{}_body.html".format(url[:-5])
+        article = HTMLDocument(url = url)
+        article.doc = self.getdoc(url)
+        article.props.headline = article.doc.cssselect("td.artheader")[0].text_content().strip()
+        article.props.text = article.doc.cssselect("table.body")[0].text_content().strip()
+        if article.doc.cssselect("td.artauthor"):
+            article.props.author = article.doc.cssselect("td.artauthor")[0].text_content().lstrip("dor")
+        else:
+            article.props.author = "-"
+        article.props.date = self.options['date']
+        return article
+        
 
 
 if __name__ == '__main__':
