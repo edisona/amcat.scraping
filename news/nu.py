e3b92e0f3b35ae309421ae2963b502bd572e12ad
Toon Alfrink <toon.alfrink@gmail.com>
2013-03-21 12:39:12 +0100

    minor haaretz fix

---
+++ news/nu.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+
+from amcat.scraping.scraper import HTTPScraper, DatedScraper
+from amcat.scraping.document import HTMLDocument
+import urllib2
+from urlparse import urljoin
+from amcat.tools.toolkit import readDate
+
+INDEX_URL = "http://www.nu.nl/zoeken/?site=nu&page={p}&q={d}+{m}+{y}"
+MONTHS = [
+    'januari',
+    'februari',
+    'maart',
+    'april',
+    'mei',
+    'juni',
+    'juli',
+    'augustus',
+    'september',
+    'oktober',
+    'november',
+    'december'
+]
+
+from urllib2 import HTTPError
+
+class NuScraper(HTTPScraper, DatedScraper):
+    medium_name = "Nu.nl"
+
+    def _get_units(self):
+        url_formats = {
+            'd' : self.options['date'].day,
+            'm' : MONTHS[self.options['date'].month-1],
+            'y' : self.options['date'].year,
+            'p' : 1
+            }
+
+        doc = self.getdoc(INDEX_URL.format(**url_formats))
+        while doc.cssselect("div.searchResultItem"):
+            for unit in doc.cssselect("div.searchResultItem"):
+                a = unit.cssselect("div.title a")[0]
+                if "NU TV Gids" in a.text_content() or not(unit.cssselect("div.updatedAt")):
+                    continue
+                
+                date =readDate(unit.cssselect("div.updatedAt")[0].text_content().lstrip("Latse upd"))
+
+                    
+                yield HTMLDocument(url=a.get('href'), headline = a.text, date = date)
+            if doc.cssselect("div.pagination"):
+                url_formats['p'] += 1
+                doc = self.getdoc(INDEX_URL.format(**url_formats))
+            else:
+                break
+                                
+
+    def _scrape_unit(self, page):
+        page.prepare(self)
+        error = page.doc.cssselect(".errorview")
+        if error:
+            return
+        try:
+            date_str = page.doc.cssselect("div.dateplace-data")[0].text_content()
+        except IndexError:
+            date_str = page.doc.cssselect("div.dateplace")[0].text_content().split(":")[1]
+        if "\n" in date_str:
+            date = readDate(date_str.split("\n")[1])
+        else:
+            date = readDate(date_str)
+        if date.date() != self.options['date']: 
+            return
+        try:
+            page.props.author = page.doc.cssselect("#leadarticle span.smallprint")[0].text.split("Door:")[1].strip()
+        except IndexError:
+            pass
+        page.props.headline = page.doc.cssselect("#leadarticle .header h1")[0].text
+        ads = page.doc.cssselect("center.articlebodyad")
+        if ads:
+            for i in range(len(ads)):
+                ads[i].drop_tree() 
+        page.props.text = page.doc.cssselect("#leadarticle .content")[0]
+        yield page
+
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.document")
+    cli.run_cli(NuScraper)
+    
---
+++ newspapers/spits.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from amcat.scraping.document import HTMLDocument
+from amcat.scraping.scraper import HTTPScraper,DatedScraper
+
+from urllib import urlencode
+from urllib2 import HTTPError
+from datetime import date
+import time
+#from amcat.tools.toolkit import readDate
+#from amcat.scraping.tools import toolkit
+
+
+class SpitsKrantScraper(HTTPScraper, DatedScraper):
+    medium_name = "Spitsnieuws krant"
+
+    cookie_url = "http://tmgonlinemedia.nl/consent/consent/?return=http%3A%2F%2Fwww.spitsnieuws.nl%2F&clienttime={timestamp}&detect=true"
+    page_url = "http://krant.spitsnieuws.nl/spits/_main_/{y:04d}/{m:02d}/{d:02d}/{pagenum:03d}/page.html"
+    article_url = "http://krant.spitsnieuws.nl/spits/_main_/{y:04d}/{m:02d}/{d:02d}/{pagenum:03d}/article/{articleid}.html"
+
+    def accept_cookies(self):
+        timestamp = int(time.time())
+        params = {
+            'clienttime' : timestamp,
+            'detect' : 'true',
+            'return' : 'http://www.spitsnieuws.nl/'
+            }
+        self.open(self.cookie_url, urlencode(params))
+
+
+    def _get_units(self):
+        self.accept_cookies()
+
+        url_dict = {
+            'y' : self.options['date'].year,
+            'm' : self.options['date'].month,
+            'd' : self.options['date'].day,
+        }
+
+        index_url = self.page_url.format(pagenum = 1, **url_dict)[:-9]
+        try:
+            index_doc = self.getdoc(index_url) 
+        except HTTPError:
+            raise ValueError("url {index_url} returns 404, perhaps it is a weekend day?".format(**locals()))
+
+        sections = self.get_sections(index_doc)
+        
+        pages = [int(option.get('value')) for option in index_doc.cssselect(".select_page option")]
+
+        for pagenum in pages:
+            if pagenum in sections.keys():
+                self.section = sections[pagenum]
+            self.pagenum = pagenum
+
+            page_url = self.page_url.format(pagenum = pagenum, **url_dict)
+            page_doc = self.getdoc(page_url)
+            article_ids = set([
+                    int(div.get('class').lstrip("article"))
+                    for div in page_doc.cssselect("#page div")
+                    ])
+
+            for url in [self.article_url.format(**dict(locals(), **url_dict))
+                        for articleid in article_ids]:
+                yield url
+
+    def get_sections(self, doc):
+        sections = {}
+
+        table = doc.cssselect("td.nav table")[0]
+        for tr in table.cssselect("> tr"):
+            if "Secties" in tr.text_content():
+                html = tr
+                break
+
+
+        for a in html.cssselect("a"):
+            pagenum = int(a.get('href').split("/")[1])
+            sections[pagenum] = a.text
+        return sections
+
+        
+    def _scrape_unit(self, url):
+        article = HTMLDocument(url = url, section = self.section)
+        article.prepare(self)
+        article.props.date = date(*[int(n) for n in url.split("/")[5:8]])
+        article.props.pagenr = self.pagenum
+        try:
+            article.props.headline = article.doc.cssselect("#article h1")[0].text_content()
+        except IndexError:
+            return
+        article.props.text = article.doc.cssselect("div.body")[0]
+        if article.doc.cssselect("#article address"):
+            article.props.author = article.doc.cssselect("#article address")[0].text_content().lstrip("dor").strip()
+
+        yield article
+
+
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping")
+    cli.run_cli(SpitsKrantScraper)
+
+
