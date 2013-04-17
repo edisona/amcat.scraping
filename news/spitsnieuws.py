28bf721d31736683a358f1dcf2818be004473775
Toon Alfrink <toon.alfrink@gmail.com>
2013-04-17 20:44:35 +0200

    fixed spitsnieuws website scraper

--- news/spitsnieuws.py
+++ news/spitsnieuws.py
@@ -19,7 +19,7 @@ from __future__ import unicode_literals, print_function, absolute_import
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
 
-INDEX_URL = "http://www.spitsnieuws.nl/archives/%(year)s%(month)02d/"
+
 
 from amcat.scraping.scraper import DatedScraper, HTTPScraper
 from amcat.scraping.document import HTMLDocument
@@ -29,17 +29,31 @@ from urlparse import urljoin
 
 class SpitsnieuwsScraper(DatedScraper, HTTPScraper):
     medium_name = "Spits - website"
-
+    index_url = "http://www.spitsnieuws.nl/archives/{self.date.year:04d}{self.date.month:02d}"
     def _get_units(self):
-        date = self.options['date']
-        url = INDEX_URL % dict(year=date.year, month=date.month)
+        self.date = self.options['date']
+        url = self.index_url.format(**locals())
+        first = self.getdoc(url)
+        for doc in self.get_pages(first):
+            for li in doc.cssselect('div.item-list-archive ul li.views-row'):
+                docdate = toolkit.readDate(li.text.strip('\n\r \u2022:')).date()
+                if docdate == todate(self.date):
+                    href = li.cssselect('a')[0].get('href')
+                    href = urljoin(url, href)
+                    yield HTMLDocument(url=href)
+                elif docdate < todate(self.date):
+                    return
 
-        for li in self.getdoc(url).cssselect('div.item-list-archive ul li.views-row'):
-            docdate = toolkit.readDate(li.text.strip('\n\r \u2022:')).date()
-            if docdate == todate(date):
-                href = li.cssselect('a')[0].get('href')
-                href = urljoin(INDEX_URL, href)
-                yield HTMLDocument(url=href)
+
+    def get_pages(self, doc):
+        page_url = self.index_url + "?page={pagenum}"
+        yield doc
+        pagenum = 1
+        last_page = int(doc.cssselect("li.pager-last a")[0].get('href').split("=")[1])
+        while pagenum <= last_page:
+            url = page_url.format(**locals())
+            yield self.getdoc(url)
+            pagenum += 1
 
     def _scrape_unit(self, doc):
         doc.doc = self.getdoc(doc.props.url)
@@ -51,7 +65,7 @@ class SpitsnieuwsScraper(DatedScraper, HTTPScraper):
         doc.props.date = toolkit.readDate(" ".join(footer[1:3]))
         for c in self.comments(doc):
             c.parent = doc
-            c,.is_comment = True
+            c.is_comment = True
             yield c
 
 
@@ -64,7 +78,7 @@ class SpitsnieuwsScraper(DatedScraper, HTTPScraper):
         for div in divs:
             comm = doc.copy()
             comm.props.headline = "re: {}".format(doc.props.headline)
-            comm.props.text = div.cssselect('p')[0]
+            comm.props.text = div.cssselect('p')
             comm.props.author = div.cssselect('strong')[0].text
 
             dt = " ".join(div.cssselect("ul")[0].text_content().split("|")[1:-1])
