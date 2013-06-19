70a8104efaf0151bc220a66c5a0ff6494d97197b
Toon Alfrink <toon.alfrink@gmail.com>
2013-06-19 12:38:25 +0200

    improvements teletekst scraper

--- teletekst.py
+++ teletekst.py
@@ -23,53 +23,77 @@ from __future__ import unicode_literals, print_function, absolute_import
 from amcat.scraping.scraper import DatedScraper, HTTPScraper
 from amcat.scraping.document import HTMLDocument
 from urlparse import urljoin
-from datetime import date
+from datetime import date, datetime
+import logging; log = logging.getLogger(__name__)
 
-INDEX_URL = "http://teletekst.mobile.nob.nl/xda-teletekst/index.jsp?page=TTARTICLE_PAGE_{page}_{tab}.htm"
-BASE_URL = "http://teletekst.mobile.nob.nl/xda-teletekst/index.jsp"
+
 
 class TeletekstScraper(HTTPScraper, DatedScraper):
     medium_name = "Teletekst"
-
-    def __init__(self, *args, **kwargs):
-        super(TeletekstScraper, self).__init__(*args, **kwargs)
+    base_url = "http://teletekst.mobile.nob.nl/xda-teletekst/index.jsp"
+    page_url = "http://teletekst.mobile.nob.nl/xda-teletekst/index.jsp?page=TTARTICLE_PAGE_{page:03d}_{tab:02d}.htm"
 
     def _get_units(self):
-        index = [INDEX_URL.format(page=101,tab="01")]
-        index_1 = self.getdoc(index[0])
-        for a in index_1.cssselect("p a"):
+        self.index_urls = [self.page_url.format(page=101, tab=1)]
+        p101 = self.getdoc(self.index_urls[0])
+        for a in p101.cssselect("p a"):
             href = a.get('href')
-            url = urljoin(BASE_URL,href)
-            yield url
+            url = urljoin(self.base_url, href)
+            self.index_urls.append(url)
+        for u in self.index_urls:
+            for a in self.getdoc(u).cssselect("a"):
+                yield a
         
-    def _scrape_unit(self, url):
-        doc = self.getdoc(url)
-        for a in doc.cssselect("a"):
-            href = a.get('href')
-            url = urljoin(BASE_URL,href)
+    def _scrape_unit(self, a):
+        url = urljoin(self.base_url, a.get('href'))
+        if url not in self.index_urls and len(a.text) == 3:
             page = HTMLDocument(date = date.today(), url = url)
+            page.props.time_scraped = datetime.now()
+            page.props.pagenr = int(a.text)
             page.prepare(self)
-            page.doc = self.getdoc(page.props.url)
-            yield self.get_article(page)
-
+            try:
+                article = self.get_article(page)
+            except ValueError:
+                log.exception("get_article failed")
+            else:
+                yield article
 
     def get_article(self, page):
-        page.props.author = ""
-        for table in page.doc.cssselect("table"):
-            table.drop_tree()
-        page.props.headline = page.doc.cssselect("title")[0].text.partition(":")[2]
-        page.props.text = page.doc.cssselect("body")[0]
-        page.coords = ""
-        
+        nextpage = False
+        for tag in page.doc.cssselect("table, img"):
+            if tag.get('alt') == "volgende":
+                nextpage = True
+            tag.drop_tree()
+        title = page.doc.cssselect("title")[0].text
+        if title == "Teletekst pagina niet beschikbaar":
+            raise ValueError(title)
+        page.props.headline = title.split(":")[1]
+        page.props.text = page.doc.cssselect("body")
+        if nextpage:
+            page.props.text += self.add_text(page.props.pagenr, 2)
         return page
 
+    def add_text(self, page, tab):
+        url = self.page_url.format(**locals())
+        doc = self.getdoc(url)
+        if self.nextpage(doc):
+            text = self.add_text(page = page, tab = tab + 1)    
+        else:
+            text = []
+        return doc.cssselect("body") + text
 
-
+    def nextpage(self, doc):
+        more = False
+        for tag in doc.cssselect("table, img"):
+            if tag.get('alt') == "volgende":
+                more = True
+            tag.drop_tree()
+        return more
+            
 
 if __name__ == '__main__':
     from amcat.scripts.tools import cli
     from amcat.tools import amcatlogging
-    amcatlogging.debug_module("amcat.scraping.scraper")
-    amcatlogging.debug_module("amcat.scraping.document")
+    amcatlogging.info_module("amcat.scraping")
     cli.run_cli(TeletekstScraper)
 
