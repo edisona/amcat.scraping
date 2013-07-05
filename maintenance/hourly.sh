a63d14718f6fdc79ed726097826e8fb6950a94ce
Toon Alfrink <toon.alfrink@gmail.com>
2013-07-05 08:38:03 +0200

    teletekst deduplication

--- maintenance/hourly.sh
+++ maintenance/hourly.sh
@@ -3,6 +3,8 @@
 DATE=$(date +'%Y-%m-%d')
 python $PYTHONPATH/scraping/teletekst.py $TELETEKST_PROJECT --articleset $TELETEKST_ARTICLESET $DATE
 wait
+python $PYTHONPATH/amcat/scripts/maintenance/deduplicate.py $TELETEKST_ARTICLESET
+wait
 kill `ps -ef |grep twitter_statuses_filter|grep -v grep|awk '{print $2}'`
 wait
 python $PYTHONPATH/scraping/social/twitter/twitter_statuses_filter.py $DATE
