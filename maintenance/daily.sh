67733cb022da5b6624cc65e5768d87e5319561aa
Toon Alfrink <toon.alfrink@gmail.com>
2013-07-09 18:25:50 +0200

    fix daily.sh

--- maintenance/daily.sh
+++ maintenance/daily.sh
@@ -4,7 +4,7 @@ DATE=$(date -d 'yesterday' +'%Y-%m-%d')
 cd $PYTHONPATH
 hg up -C trunk
 cd $PYTHONPATH/scraping
-hg up -C trunk
+hg up -C defauly
 wait
 python $PYTHONPATH/amcat/scripts/maintenance/daily.py $DATE --deduplicate True 2>&1
 wait
