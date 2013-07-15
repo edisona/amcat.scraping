b1b996183a3398a412645663c3aad9ec1dd33820
Toon Alfrink <toon.alfrink@gmail.com>
2013-07-15 12:30:55 +0200

    maintenance updates for scraping

--- maintenance/midnight.sh
+++ maintenance/midnight.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DATE=$(date -d 'yesterday' +'%Y-%m-%d')
-python $PYTHONPATH/scrapers/vienna/krone.py $KRONE_PROJECT $DATE --articleset $KRONE_ARTICLESET
+python $PYTHONPATH/scrapers/vienna/krone.py $VIENNA_PROJECT $DATE --articleset $KRONE_ARTICLESET
