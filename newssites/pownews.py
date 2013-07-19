62c0eec7e41eeaa2af7cef6bc10d21d375dd65d6
Toon Alfrink <toon.alfrink@gmail.com>
2013-07-19 10:18:12 +0200

    gmx scraper

---
+++ newssites/pownews.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+
+from urlparse import urljoin
+from urllib2 import HTTPError
+from datetime import date, timedelta
+
+from amcat.scraping.document import Document, HTMLDocument
+from amcat.tools.toolkit import readDate
+
+BASE_URL = "http://www.powned.tv"
+ARCHIVE_URL = "http://www.powned.tv/archief/{d.year:04d}/{d.month:02d}/{x:02d}-week/"
+
+from amcat.scraping.scraper import HTTPScraper,DatedScraper
+
+class PownewsScraper(HTTPScraper, DatedScraper):
+    medium_name = "PowNews"
+
+    def _get_units(self):
+        self.open("http://www.powned.tv")
+        self.open("http://cookies.publiekeomroep.nl/accept/")
+        d = self.options['date']
+        #search for the right archive page
+        for x in range(d.day - 7, d.day + 7):
+            archive_url = ARCHIVE_URL.format(**locals())
+            try:
+                documents = [self.getdoc(archive_url)]
+            except HTTPError:
+                pass
+            else:
+                break
+
+        #get last and next week too
+        page_date = date(self.options['date'].year, self.options['date'].month, x)
+        for d in [page_date - timedelta(days=7), page_date + timedelta(days=7)]:
+            x = d.day
+            url = ARCHIVE_URL.format(**locals())
+            documents.append(self.getdoc(url))
+
+        #find articles with right date and yield
+        for doc in documents:
+            for li in doc.cssselect("ul.articlelist li"):
+                from lxml import html
+                datestr = " ".join(li.cssselect("span.t")[0].text.split()[:2]) + " " + str(self.options['date'].year)
+                _date = readDate(datestr).date()
+                print(_date)
+                print(_date == self.options['date'])
+                if _date == self.options['date']:
+                    article = HTMLDocument(date = _date,
+                                           url = urljoin(archive_url, li.cssselect("a")[0].get('href')))
+                    yield article
+
+    def _scrape_unit(self, article):        
+        article.prepare(self)
+        article = self.get_article(article)
+        for comment in self.get_comments(article):
+            comment.is_comment = True
+            yield comment
+        yield article
+
+    def get_article(self, page):
+        page.props.author = page.doc.cssselect("#artikel-footer .author-date")[0].text.split("|")[0].strip()
+        page.props.headline = page.doc.cssselect("div.acarhead h1")[0].text
+        page.props.text = [page.doc.cssselect("div.artikel-intro")[0], page.doc.cssselect("div.artikel-main")[0]]
+        page.props.section = page.props.url.split("/")[4]
+        return page
+
+    def get_comments(self,page):
+        for div in page.doc.cssselect("#comments div.comment"):
+            comment = HTMLDocument(parent = page)
+            comment.props.section = page.props.section
+            comment.props.url = page.props.url
+            comment.props.text = div.cssselect("p")[0]
+            footer = div.cssselect("p.footer")[0].text_content().split(" | ")
+            comment.props.author = footer[0].strip()
+            comment.props.date = readDate(footer[1].strip())
+            yield comment
+                                         
+
+
+
+
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.document")
+    cli.run_cli(PownewsScraper)
+
+
---
+++ newssites/volkskrant.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+import re
+
+from amcat.scraping.document import Document, HTMLDocument
+
+from urlparse import urljoin
+from urllib2 import HTTPError
+from amcat.tools.toolkit import readDate
+from datetime import datetime
+from amcat.scraping.htmltools import create_cc_cookies
+
+INDEX_URL = "http://www.volkskrant.nl/vk/nl/2/archief/integration/nmc/frameset/archive/archiveDay.dhtml?archiveDay={y:04d}{m:02d}{d:02d}"
+
+from amcat.scraping.scraper import HTTPScraper,DatedScraper
+
+class WebVolkskrantScraper(HTTPScraper, DatedScraper):
+    medium_name = "Volkskrant website"
+
+    def set_cookies(self):
+        for cookie in create_cc_cookies(".volkskrant.nl"):
+            self.opener.cookiejar.set_cookie(cookie)
+
+    def _get_units(self):
+        self.set_cookies()
+        index_dict = {
+            'y':self.options['date'].year,
+            'm':self.options['date'].month,
+            'd':self.options['date'].day
+            }
+        index = self.getdoc(INDEX_URL.format(**index_dict))
+        for unit in index.cssselect('#hvdn_archief dd'):
+            url = unit.cssselect("a")[0].get('href')
+            unit.cssselect("span")[0].drop_tree()
+            title = unit.cssselect("a")[0].text_content().strip("\"")
+            yield HTMLDocument(url=url,headline=title)
+ 
+        
+    def _scrape_unit(self, page): 
+        page.prepare(self)
+        try:
+            author = page.doc.cssselect("span.author")[0]
+            if "OPINIE" in author.text:
+                page.props.author = author.text.split("-")[1].strip()
+            elif "Door:" in author.text:
+                page.props.author = author.text.split("Door:")[1].strip()
+            else:
+                page.props.author = author.text
+        except IndexError:
+            try:
+                time_post = page.doc.cssselect("div.time_post")[0].text
+            except IndexError:
+                return
+            if "bron" in time_post:
+                page.props.author = page.doc.cssselect("div.time_post")[0].text.split("bron:")[1]
+            else:
+                page.props.author = "None"
+        if hasattr(page.props,"author"):
+            if page.props.author:
+                page.props.author = page.props.author[:98]
+        page.props.text = page.doc.cssselect("#art_box2")[0]
+        page.props.section = re.search("/vk/nl/[0-9]+/([a-zA-Z\-]+)/article/", page.props.url).group(1)
+        yield page
+
+
+
+
+if __name__ == '__main__':
+    from amcat.scripts.tools import cli
+    from amcat.tools import amcatlogging
+    amcatlogging.debug_module("amcat.scraping.scraper")
+    amcatlogging.debug_module("amcat.scraping.document")
+    cli.run_cli(WebVolkskrantScraper)
